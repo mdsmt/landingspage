3a9065227418f653a732cc7c91f0d0f1657b6cc1
mdsmt <mitchell.desmet@gmail.com>
2014-03-21 18:07:00 +0100

    Files landingspage V1.2

---
+++ contact/thanks.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Mitchell Desmet - Designer, Developer, Partyanimal.</title>
+	<link rel="stylesheet" href="css/reset.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link href='http://fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+</head>
+<body>
+	<header>
+		
+	</header> <!-- Close header -->
+	<div id="titel">
+		<h1>Mitchell Desmet</h1>
+	</div>
+	<div id="content">
+		<h1>Bedankt!</h1>
+		<p> De mail is verzonden.<br />
+		  Bedankt!</p>
+
+	<p><a href="http://mdsmt.be">Terug</a></p>
+
+	</div>
+
+	
+</body>
+</html>
